0d0a56002334614a338a1887be6f2c8c88783aa7
Michelangelo <dragonbe+github@gmail.com>
2015-02-01 15:55:07 +0100

    Initial test

---
+++ tests/Phpqa/Model/CommentTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Phpqa\Tests\Model;
+
+class CommentTest extends \PHPUnit_Framework_TestCase
+{
+    public function testModelIsPopulatedAtConstruct()
+    {
+        $faker = \Faker\Factory::create();
+        $data = [
+            'commentId' => rand(1, time()),
+            'fullName' => $faker->name,
+            'emailAddress' => $faker->email,
+            'website' => $faker->url,
+            'comment' => $faker->text(),
+        ];
+
+        $comment = new \Phpqa\Model\Comment($data);
+        $this->assertSame($data['commentId'], $comment->getCommentId());
+        $this->assertSame($data['fullName'], $comment->getFullName());
+        $this->assertSame($data['emailAddress'], $comment->getEmailAddress());
+        $this->assertSame($data['website'], $comment->getWebsite());
+        $this->assertSame($data['comment'], $comment->getComment());
+    }
+}
