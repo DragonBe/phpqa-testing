bd9a56b0a3cd4651be8c760f0a069ac84e362271
Michelangelo <dragonbe+github@gmail.com>
2015-02-01 19:25:08 +0100

    Implementing dataprovider for processing good data

--- tests/Phpqa/Model/CommentTest.php
+++ tests/Phpqa/Model/CommentTest.php
@@ -5,22 +5,36 @@ use Phpqa\Model\Comment;
 
 class CommentTest extends \PHPUnit_Framework_TestCase
 {
-    public function testModelIsPopulatedAtConstruct()
+    /**
+     * @dataProvider goodDataProvider
+     */
+    public function testModelIsPopulatedAtConstruct($data)
     {
-        $faker = \Faker\Factory::create();
-        $data = [
-            'commentId' => rand(1, time()),
-            'fullName' => $faker->name,
-            'emailAddress' => $faker->email,
-            'website' => $faker->url,
-            'comment' => $faker->text(),
-        ];
-
         $comment = new Comment($data);
         $this->assertSame($data['commentId'], $comment->getCommentId());
         $this->assertSame($data['fullName'], $comment->getFullName());
         $this->assertSame($data['emailAddress'], $comment->getEmailAddress());
         $this->assertSame($data['website'], $comment->getWebsite());
         $this->assertSame($data['comment'], $comment->getComment());
+    }
+
+    /**
+     * Provides data that we consider to be safe and of quality
+     * @return array
+     */
+    public function goodDataProvider()
+    {
+        $faker = \Faker\Factory::create();
+        $data = [];
+        for ($iter = 0; $iter < 50; $iter++) {
+            $data[] = [
+                'commentId' => rand(1, time()),
+                'fullName' => $faker->name,
+                'emailAddress' => $faker->email,
+                'website' => $faker->url,
+                'comment' => $faker->text(),
+            ];
+        }
+        return $data;
     }
 }
